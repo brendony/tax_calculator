c657a0d319b25655ded32e6cf74ee602fc1e0a40
Brendon Yenson <20347784+brendony@users.noreply.github.com>
2024-12-14 22:16:50 +1100

    Add the min_income info to the brackets. It's information that could be inferred, but it's better to be stated explicitly (and might be reported in a table)

--- controller/tax_system.py
+++ controller/tax_system.py
@@ -7,14 +7,13 @@ class TaxSystem:
 
     @staticmethod
     def calculate_tax(fy: str, income: int) -> float:
-        # We should store these in decreasing order, but:
-        # Because there are only 5 or six brackets per year, I'm going to sort them
-        # to make sure things are correct
-        brackets = sorted(TaxSystem.get_brackets_for_year(fy), key=lambda x: x.max_income or math.inf)
+        if income < 0:
+            return 0
+        brackets = TaxSystem.get_brackets_for_year(fy)
         if len(brackets) == 0:
             raise Exception(f'Unknown FY {fy}, must be one of {historical_tax_rates.keys()}')
-        bracket = [bracket for bracket in brackets
-                   if income < (bracket.max_income or math.inf)][0]
+        bracket = next(bracket for bracket in brackets
+                       if bracket.min_income <= income <= (bracket.max_income or math.inf))
         return bracket.base_amount + bracket.cents_per_dollar / 100.0 * (int(income) - bracket.over_amount)
 
     @staticmethod
--- model/tax_rates.py
+++ model/tax_rates.py
@@ -1,6 +1,5 @@
 # The tax rate information might come as a data file (json, csv, even excel) but for now I'm just going to use
 # python to simplify things for myself.
-
 from typing import Optional
 
 from attr import dataclass
@@ -21,7 +20,8 @@ class TaxBracket:
     # - centsPerDollar is 32.5
     # - overAmount is 45000
 
-    max_income: Optional[int]
+    min_income: int
+    max_income: Optional[int]  # if max_income is None, it means any income > min_income is in this bracket
     base_amount: float
     cents_per_dollar: float
     over_amount: int
@@ -29,17 +29,19 @@ class TaxBracket:
 
 historical_tax_rates: dict[str, list[TaxBracket]] = {
     '2020-2021': [
-        TaxBracket(max_income=18200, base_amount=0, cents_per_dollar=0.0, over_amount=0),
-        TaxBracket(max_income=45000, base_amount=0, cents_per_dollar=19, over_amount=18200),
-        TaxBracket(max_income=120000, base_amount=5092, cents_per_dollar=32.5, over_amount=45000),
-        TaxBracket(max_income=180000, base_amount=29467, cents_per_dollar=37, over_amount=120000),
-        TaxBracket(max_income=None, base_amount=51667, cents_per_dollar=45, over_amount=180000)
+        TaxBracket(min_income=0, max_income=18200, base_amount=0, cents_per_dollar=0.0, over_amount=0),
+        TaxBracket(min_income=18201, max_income=45000, base_amount=0, cents_per_dollar=19, over_amount=18200),
+        TaxBracket(min_income=45001, max_income=120000, base_amount=5092, cents_per_dollar=32.5, over_amount=45000),
+        TaxBracket(min_income=120001, max_income=180000, base_amount=29467, cents_per_dollar=37, over_amount=120000),
+        TaxBracket(min_income=180001, max_income=None, base_amount=51667, cents_per_dollar=45,
+                   over_amount=180000)
     ],
     '2021-2022': [  # NB: this is the same as 2020-2021, but I'm leaving them here as it is explicit
-        TaxBracket(max_income=18200, base_amount=0, cents_per_dollar=0.0, over_amount=0),
-        TaxBracket(max_income=45000, base_amount=0, cents_per_dollar=19, over_amount=18200),
-        TaxBracket(max_income=120000, base_amount=5092, cents_per_dollar=32.5, over_amount=45000),
-        TaxBracket(max_income=180000, base_amount=29467, cents_per_dollar=37, over_amount=120000),
-        TaxBracket(max_income=None, base_amount=51667, cents_per_dollar=45, over_amount=180000)
+        TaxBracket(min_income=0, max_income=18200, base_amount=0, cents_per_dollar=0.0, over_amount=0),
+        TaxBracket(min_income=18201, max_income=45000, base_amount=0, cents_per_dollar=19, over_amount=18200),
+        TaxBracket(min_income=45001, max_income=120000, base_amount=5092, cents_per_dollar=32.5, over_amount=45000),
+        TaxBracket(min_income=120001, max_income=180000, base_amount=29467, cents_per_dollar=37, over_amount=120000),
+        TaxBracket(min_income=180001, max_income=None, base_amount=51667, cents_per_dollar=45,
+                   over_amount=180000)
     ]
 }
